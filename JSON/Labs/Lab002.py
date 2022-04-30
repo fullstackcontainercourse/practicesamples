deb837502f2615108e6390fb85b62bf0a823ad67
unraveller <skpraveen18@gmail.com>
2022-04-30 22:32:00 +0800

    Lab002

---
+++ JSON/Labs/Lab002.py
@@ -0,0 +1,21 @@
+## Topic : JSON
+# Exercise 002
+
+""" 
+Problem Statement : 
+Read the student dictionary as a json string and print the student details.
+
+Approach :
+Complete the following program and make sure the test cases in TestLab002.py passes successfully.
+
+"""
+import json
+
+student_dict = {'name': 'Gil', 'age': 20,
+                'subjects': ['IT', 'OOPS', 'MATHS', 'DS']}
+
+
+def getStudentDataAsJson():
+    # Read the dictionary as String into student_data_json variable.
+
+    return student_data_json
