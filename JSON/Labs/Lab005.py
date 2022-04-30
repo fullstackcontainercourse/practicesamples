1844cac6eca40d856415f72b50c9eb423890b745
unraveller <skpraveen18@gmail.com>
2022-04-30 23:23:42 +0800

    Lab5

---
+++ JSON/Labs/Lab005.py
@@ -0,0 +1,27 @@
+## Topic : JSON
+# Exercise 005
+
+""" 
+Problem Statement : 
+Ehnance the Employee.json with Project details as an ARRAY of objects inside Employee.
+Each Project can have attributes as
+1) name (String)
+2) tenure (int)
+3) is_internal (boolean)
+
+# hint : this is based on Lab 004's solution.
+
+Approach :
+Complete the following program and make sure the test cases in TestLab005.py passes successfully/
+
+"""
+
+import json
+
+
+def readEmployeeFromJSONFile():
+    # Write code to open the file
+
+    # Load json data as python object.
+
+    return employee_data
