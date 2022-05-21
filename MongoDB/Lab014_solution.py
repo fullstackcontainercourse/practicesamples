78e2f73275c8c4366831dc2ce6d0a411457026c7
unraveller <skpraveen18@gmail.com>
2022-05-21 22:36:44 +0800

    Lab14

---
+++ MongoDB/Lab014_solution.py
@@ -0,0 +1,56 @@
+import unittest
+from mongosetup import *
+
+
+'''
+Description : Complete the below method using MongoDB methods to insert a 'Book' document into
+'Library' Database.
+
+Points to note :
+Try to use collections.insert(dictionary) method
+The document is available only after it is inserted in the DB
+
+
+'''
+
+
+def createQueryToReadBooksData(isbnno):
+    query = {"isbn": isbnno}
+
+    return query
+
+
+def createBookDocuments():
+
+    # connect to 'Library' data base in MongoDB
+
+    # connect to 'Books' collection
+
+    # user insertOne() method to create the document in MongoDB
+
+    inserted_doc_id = getCollectionsFromLibraryDB("Books").insert_many([
+        {"title": "Seven Eleven", "author": "John Peter", "isbn": "67234"},
+        {"title": "The Alice in Wonderland",
+            "author": "James Wales", "isbn": "134543"},
+        {"title": "Seven Stages", "author": "Mike Hary", "isbn": "78564"},
+        {"title": "Fairy Tales", "author": "Jim Magats", "isbn": "123"},
+        {"title": "The Truth", "author": "Blandina Paul", "isbn": "85234"}])
+
+    return inserted_doc_id
+
+
+class TestLab14(unittest.TestCase):
+    def test_LibraryBooksCollection(self):
+        # call create Book document
+        createBookDocuments()
+        # Test the created record
+        books_data = getCollectionsFromLibraryDB(
+            "Books").find(createQueryToReadBooksData(78564))
+        print(books_data)
+
+        self.assertIsNotNone(
+            books_data, "No Books data found. It is suppose to be found.")
+
+
+if __name__ == '__main__':
+    unittest.main()
